28dc0a8c7a29f8266ea3af0a10dde2e9e6eb4b08
Shiva552003 <tejasworld3@gmail.com>
2024-07-02 16:41:01 +0530

    more css and db changes

--- routes_influ.py
+++ routes_influ.py
@@ -1,10 +1,27 @@
-from flask import render_template
+from flask import render_template,session, url_for,redirect,send_file
 from main import app
+from model import Influencer
+import io
 
 
 @app.route('/home/influencer')
 def influ_home():
-    return render_template('influencer/home_influ.html', active='home')
+    influ=Influencer.query.filter_by(id=session['userId']).first()
+    cover_photo= url_for('get_cover_photo_influ', influ_id=influ.id)
+
+    return render_template('influencer/home_influ.html', active='home',influ=influ,cover_photo=cover_photo)
+
+@app.route('/sponsor/<int:influ_id>/cover_photo')
+def get_cover_photo_influ(influ_id):
+    influencer = Influencer.query.get_or_404(influ_id)
+    if influencer.cover_photo:
+        return send_file(
+            io.BytesIO(influencer.cover_photo),
+            mimetype='image/jpeg',
+            as_attachment=False,
+        )
+    else:
+        return redirect(url_for('home'))
 
 @app.route('/find/influencer')
 def influ_find():
--- routes_spon.py
+++ routes_spon.py
@@ -1,18 +1,21 @@
-from flask import render_template,session,send_file,redirect,url_for
+from flask import render_template,session,send_file,redirect,url_for,request,flash
 from main import app
-from model import Sponsor
+from model import Ad_request, Sponsor,Campaign,db
 from wrappers import sponsor_required
+from datetime import datetime
 import io
 
 
 @app.route('/home/sponsor')
 @sponsor_required
 def spon_home():
-    spon=Sponsor.query.filter_by(id=session['userId']).first();
+    spon=Sponsor.query.filter_by(id=session['userId']).first()
     cover_photo= url_for('get_cover_photo', sponsor_id=spon.id)
 
+    campaigns=Campaign.query.filter_by(spon_id=spon.id).all()
+    requests=Ad_request.query.filter_by(spon_id=spon.id).all()
 
-    return render_template('sponsor/home_spon.html', active='home', spon=spon,cover_photo=cover_photo)
+    return render_template('sponsor/home_spon.html', active='home', spon=spon,cover_photo=cover_photo,campaigns=campaigns,requests=requests)
 
 @app.route('/sponsor/<int:sponsor_id>/cover_photo')
 def get_cover_photo(sponsor_id):
@@ -31,6 +34,54 @@ def get_cover_photo(sponsor_id):
 @sponsor_required
 def spon_campaigns():
     return render_template('sponsor/campaigns.html', active='campaigns')
+
+@app.route('/sponsor/add_campaign')
+@sponsor_required
+def add_campaigns():
+    camp={}
+    return render_template('sponsor/add_campaign.html', active='campaigns',camp=camp)
+
+@app.route('/sponsor/add_campaign', methods=['POST'])
+@sponsor_required
+def add_campaigns_Post():
+    name = request.form.get('name_html')
+    budget = request.form.get('budget_html')
+    desc = request.form.get('desc_html')
+    industry = request.form.get('industry_html')
+    s_date = request.form.get('start_date_html')
+    e_date = request.form.get('end_date_html')
+    goal = request.form.get('goal_users_html')
+    visibility = request.form.get('visibility')
+
+    camp={'name':name, 'budget':budget,'desc':desc,'industry': industry,'start_date':s_date,'end_date':e_date,'status':visibility}
+
+    s_date = datetime.strptime(s_date, '%Y-%m-%d').date() if s_date else None
+    e_date = datetime.strptime(e_date, '%Y-%m-%d').date() if e_date else None
+    
+    if not name or not budget or not industry or not s_date:
+        flash('Please enter all the required fields','danger')
+        return render_template('sponsor/add_campaign.html', active='campaigns',camp=camp)
+    else:
+        campaign = Campaign(
+            spon_id=session['userId'],
+            name=name,
+            budget=budget,
+            description=desc,
+            category=industry,
+            start_date=s_date,
+            end_date=e_date,
+            goal_users=goal,
+        )
+        db.session.add(campaign)
+        db.session.commit()
+        # return redirect(url_for('spon_view_camp',camp_id=campaign.id))
+        return redirect(url_for('spon_view_camp'))
+
+@app.route('/sponsor/view_campaign')
+@sponsor_required
+def spon_view_camp():
+    # camp = Campaign.query.filter_by(id=camp_id).first()
+    return render_template('sponsor/view_camp.html', active='campaigns', camp={})
 
 @app.route('/find/sponsor')
 @sponsor_required
